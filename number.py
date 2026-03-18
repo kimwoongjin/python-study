20f66deb9ff4a85519969f91f528f3f54ec6aa99
kimwoongjin <woong1536@gmail.com>
2026-03-18 16:46:41 +0900

    text, number

---
+++ number.py
@@ -0,0 +1,21 @@
+my_name = 'Python'
+my_age = 25
+
+print(my_name, '은 이제', my_age, '살')
+
+my_next_age = my_age + 1
+
+print('내년에는', my_next_age, '살')
+
+multiply = 9 * 9
+divide = 30 / 5
+power = 2 ** 10
+remainder = 15 % 4
+
+print(multiply, divide, power, remainder)
+
+text = '2025' + '1991'
+number = 2025 + 1991
+
+print(text)
+print(number)
