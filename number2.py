1a3cf635152d8e3b6371244ba11ef032483c7d77
kimwoongjin <woong1536@gmail.com>
2026-03-20 15:55:12 +0900

    정수와 실수

---
+++ number2.py
@@ -0,0 +1,22 @@
+five1 = 5
+five2 = 5.0
+five3 = 5.0000
+
+print(five1)
+print(five2)
+print(five3)
+
+div1 = 6 / 5
+div2 = 6 // 5
+div3 = 6 % 5
+
+print(div1)
+print(div2)
+print(div3)
+
+print(0.1 + 0.1 == 0.2) # True
+print (0.1 + 0.1 + 0.1 == 0.3) # False
+
+print(int(5)) # 정수로
+print(float(5)) # 부동소수점으로
+print(5 * 1.0) # 부동소수점으로
